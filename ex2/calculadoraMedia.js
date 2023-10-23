58888d67c1738d8912292813313ae4a94847c2a7
Eduardo J. X. L. Filho <eduardojxavier@gmail.com>
2023-10-23 17:53:12 -0300

    Signed-off-by: Eduardo J. X. L. Filho <eduardojxavier@gmail.com>

---
+++ ex2/calculadoraMedia.js
@@ -0,0 +1,7 @@
+var nota1 = parseFloat(prompt("Digite a primeira nota: "));
+var nota2 = parseFloat(prompt("Digite a segunda nota: "));
+var nota3 = parseFloat(prompt("Digite a terceira nota: "));
+
+var media = (nota1+nota2+nota3)/3
+
+alert(media);
